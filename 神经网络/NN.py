3847b39d9620bf4a2850bca5968224ba5985235a
唐都 <7277710@qq.com>
2019-10-15 18:01:07 +0800

    1.commit

--- 神经网络/NN.py
+++ 神经网络/NN.py
@@ -1,55 +1,47 @@
+# 完整神经网络样例程序
 import tensorflow as tf
-import numpy as np
+from numpy.random import RandomState
 
-BATCH_SIZE = 8 #每次喂给神经网络多少组数据
+# 1. 定义神经网络的参数，输入和输出节点。
+batch_size = 8
+w1 = tf.Variable(tf.random_normal([2, 3], stddev=1, seed=1))
+w2 = tf.Variable(tf.random_normal([3, 1], stddev=1, seed=1))
+x = tf.placeholder(tf.float32, shape=(None, 2), name="x-input")
+y_ = tf.placeholder(tf.float32, shape=(None, 1), name='y-input')
 
-seed = 23455
-
-#基于seed 产生随机数
-rdm = np.random.mtrand.RandomState(seed)
-#随机生成32行2列矩阵，表示32组特征集合(假设为体积和重量)作为输入数据集
-X = rdm.rand(32, 2)
-# print()
-
-#遍历，x0 + x1 < 1 Y 赋值为1，否则 Y 赋值为0
-Y = 0
-for (x0,x1) in X:
-    if x0 + x1 < 1:
-        Y = 1
-    else:
-        Y = 0
-# Y = [[int(x0 + x1 < 1)]]for(x0,x1) in X]
-
-#定义神经网络输入、参数、输出。定义前向传播过程
-x = tf.placeholder(tf.float32,shape=(None,2)) #只知道体积和体重，None 表示不知道可以拿到多少组特征
-y_ = tf.placeholder(tf.float32,shape=(None,1)) #只知道特征是是否合格，不知道可以拿到多少组合格与否的特征(零件是否合格)
-w1 = tf.Variable(tf.random_normal([2,3],stddev = 1, seed = 1))
-w2 = tf.Variable(tf.random_normal([3,1],stddev = 1, seed = 1))
-
+# 2. 定义前向传播过程，损失函数及反向传播算法。
 a = tf.matmul(x, w1)
 y = tf.matmul(a, w2)
-#选用均方误差定义损失函数
-loss = tf.reduce_mean(tf.square(y-y_))
-#选用梯度下降实现训练过程,学习率为0.0001（也可以选择Momentum和Adam）
-train_step = tf.train.GradientDescentOptimizer(0.0001).minimize(loss)
-#生成会话
+cross_entropy = -tf.reduce_mean(y_ * tf.log(tf.clip_by_value(y, 1e-10, 1.0)))
+train_step = tf.train.AdamOptimizer(0.001).minimize(cross_entropy)
+
+# 3. 生成模拟数据集。
+rdm = RandomState(1)
+X = rdm.rand(128, 2)
+Y = [[int(x1 + x2 < 1)] for (x1, x2) in X]
+
+# 4. 创建一个会话来运行TensorFlow程序。
 with tf.Session() as sess:
-    init_op = tf.global_variables_initializer() #初始化数据
+    init_op = tf.global_variables_initializer()
     sess.run(init_op)
-    #输出未训练的参数
-    print ("w1:\n",sess.run(w1))
-        print ("w2:\n",sess.run(w2))
-        
-        #训练模型
-        STEPS = 3000
-            for i in range(STEPS):
-                start = (i * BATCH_SIZE) % 32
-                end = start + BATCH_SIZE
-            sess.run(train_step,feed_dict={x:X[start,end],y_:Y[start,end]})
-            # sess.run(train_step, feed_dict={x: X.index(X,beg=start,end=end), y_: Y.index(Y,beg=start,end=end)})
-            total_loss = sess.run(loss,feed_dict={x:X,y_:Y})
-            print("after %d training step(s),loss loss is %g" % (i,total_loss))
-                #输出训练后的参数取值
-                print ("\n")
-                print ("w1:\n",sess.run(w1))
-                print ("w1:\n",sess.run(w2))
+    
+    # 输出目前（未经训练）的参数取值。
+    print("w1:", sess.run(w1))
+    print("w2:", sess.run(w2))
+    print("\n")
+    
+    # 训练模型。
+    STEPS = 5000
+    for i in range(STEPS):
+        start = (i * batch_size) % 128
+        end = (i * batch_size) % 128 + batch_size
+        sess.run(train_step, feed_dict={x: X[start:end], y_: Y[start:end]})
+        if i % 1000 == 0:
+            total_cross_entropy = sess.run(cross_entropy, feed_dict={x: X, y_: Y})
+            print("After %d training step(s), cross entropy on all data is %g" % (i, total_cross_entropy))
+
+# 输出训练后的参数取值。
+print("\n")
+print("w1:", sess.run(w1))
+print("w2:", sess.run(w2))
+
